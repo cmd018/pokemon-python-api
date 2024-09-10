85f53af9c47c1d83a929820f4752797022030af8
Dheeraj <cmd018@gmail.com>
2024-09-10 10:04:35 +0100

    add architecture and test coverage

--- tests/test_mock_database.py
+++ tests/test_mock_database.py
@@ -16,10 +16,10 @@ class TestMockAuthentication:
         mock_no_authentication()
 
     @pytest.mark.anyio
-    async def test_mock_databases(self, client_test: AsyncClient):
+    async def test_mock_pokemons(self, client_test: AsyncClient):
         # generate data
         await Pokemon(
-            pokemonId=1,
+            pokemonId=100,
             name="ivysaur",
             image="http://some/url"
         ).create()
@@ -30,22 +30,60 @@ class TestMockAuthentication:
 
 
     @pytest.mark.anyio
-    async def test_mock_database(self, client_test: AsyncClient):
-        await Admin(
-            fullname="admin", email="admin@admin.com", password="admin"
-        ).create()
+    async def test_mock_pokemon_verify(self, client_test: AsyncClient):
 
         await Pokemon(
-            pokemonId=1,
+            pokemonId=101,
             name="ivysaur",
             image="http://some/url"
         ).create()
 
         data = {
             "name": "ivysaur", 
-            "pokemonId": 1
+            "pokemonId": 101
         }
 
         response = await client_test.post("pokemon/verify", data=json.dumps(data))
+
+        assert response.status_code == 200
+
+    @pytest.mark.anyio
+    async def test_mock_pokemon_random(self, client_test: AsyncClient):
+
+        response = await client_test.get("pokemon/random")
+
+        assert response.status_code == 200
+
+    @pytest.mark.anyio
+    async def test_mock_pokemon_random_with_id(self, client_test: AsyncClient):
+
+        response = await client_test.get("pokemon/random?id=3")
+
+        assert response.status_code == 200
+    
+    @pytest.mark.anyio
+    async def test_mock_admin_signup(self, client_test: AsyncClient):
+
+        # email already exists
+        data = {
+            "email": "email@gmail.com",
+            "fullname": "Jon Snow",
+            "password": "3xt3m#"
+        }
+
+        response = await client_test.post("admin", data=json.dumps(data))
+
+        assert response.status_code == 409
+
+    @pytest.mark.anyio
+    async def test_mock_admin_login(self, client_test: AsyncClient):
+
+        # email already exists
+        data = {
+            "password": "3xt3m#",
+            "username": "email@gmail.com"
+        }
+
+        response = await client_test.post("admin/login", data=json.dumps(data))
 
         assert response.status_code == 200
